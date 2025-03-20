0adbe8c98c27746a461327a42bd36e5a63cff156
iwan <iwancuah13@gmail.com>
2025-03-20 23:14:28 +0700

    get Appointment

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const UsersRoutes = require('./src/api/users/routes')
 const DoctorsRoutes = require('./src/api/doctors/routes')
 const BedsRoutes = require('./src/api/beds/routes')
 const LaboratoryRoutes = require('./src/api/Laboratory/routes')
+const RadiologyRoutes = require('./src/api/radiology/routes')
+const AppointmentRoutes = require('./src/api/appointment/routes')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
@@ -17,6 +19,8 @@ app.use('/Users', UsersRoutes)
 app.use('/Doctors', DoctorsRoutes)
 app.use('/Beds', BedsRoutes)
 app.use('/Laboratory', LaboratoryRoutes)
+app.use('/Radiology', RadiologyRoutes)
+app.use('/Appointment', AppointmentRoutes)
 
 app.get('*', (_, res) => {
   res.send('Backend Application for PoliQu')
